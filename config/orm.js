1f075c6ff5b9eb673245e74a326ace0d01f85cc1
Nes <epmjj4@hotmail.com>
2021-04-02 11:32:00 -0400

    finished orm.js

---
+++ config/orm.js
@@ -0,0 +1,50 @@
+//Import MySql connection object
+const connection = require('./connection');
+
+// variable to set object realtional mapping(orm)
+
+const orm = {
+    //function to set all burgers from the database
+    selectAll: (tableName, callback) => {
+        const querySelector = `SELECT * FROM ${tableName};`;
+        connection.query(querySelector, (err, results) => {
+            if(err){
+                throw err;
+            }
+            callback(results);
+        })
+    },
+    // function to add burger to the db from inpu
+    addOne: (tableName, column, value, callback) => {
+        const querySelector = `INSERT INTO ${tableName} (${column}) VALUES ('${value}');`;
+        connection.query(querySelector, (err, results) => {
+            if(err){
+                throw err;
+            }
+            callback(results);
+        })
+    }, 
+    updateOne: (columnVal, id, callback) => {
+        const querySelector = `UPDATE burgers SET devoured ='1' WHERE ${columnVal}=${id};`;
+        connection.query(querySelector, [id], (err, results) => {
+            if(err){
+                throw err;
+            }
+            callback(results);
+        })
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = orm;
