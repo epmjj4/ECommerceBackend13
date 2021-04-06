8c3001dd947a2a36b5d94fb2256b99549b2e8a2c
Nes <epmjj4@hotmail.com>
2021-04-06 18:19:01 -0400

    updated mysql file with JAWSDB

--- config/mysql-connection.js
+++ config/mysql-connection.js
@@ -1,14 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host:'localhost',
-    port:3306,
-    user: 'root',
-    password:'password',
-    database:'burgershop_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-connection.connect( err => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'password',
+        database: 'burgershop_db'
+    });
+}
+connection.connect(err => {
     if (err) {
         //Will show the stack trace of the error in mysql
         console.log(`There's an error connecting ${err.stack}`)
